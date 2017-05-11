5ef7ae5a866e43e296e10949fc96b41372f72c40
Nick0603 <b10303008@gmail.com>
2017-05-11 15:50:55 +0800

    修正bug ： 圖片載入位置

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 // 創造 img HTML 元素，並放入變數中
 var bgImg = document.createElement("img");
 // 設定這個元素的要顯示的圖片
-bgImg.src = "../images/map2.png";
+bgImg.src = "images/map2.png";
 
 
 // 創造 img HTML 元素，並放入變數中
 var heroImg = document.createElement("img");
 // 設定這個元素的要顯示的圖片
-heroImg.src = "../images/jason.gif";
+heroImg.src = "images/jason.gif";
 
 // 創造 img HTML 元素，並放入變數中
 var towerBtn = document.createElement("img");
 // 設定這個元素的要顯示的圖片
-towerBtn.src = "../images/tower-btn.png";
+towerBtn.src = "images/tower-btn.png";
 
 // 創造 img HTML 元素，並放入變數中
 var towerImg = document.createElement("img");
 // 設定這個元素的要顯示的圖片
-towerImg.src = "../images/tower.png";
+towerImg.src = "images/tower.png";
 
 // 找出網頁中的 canvas 元素
 var canvas = document.getElementById("game-canvas");
